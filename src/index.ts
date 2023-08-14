1197a4033f3bedc2c2fb5957bc1c9665a53fad95
Luca Del Puppo <luca@delpuppo.net>
2023-08-14 20:04:23 +0200

    async generators

--- src/index.ts
+++ src/index.ts
@@ -12,55 +12,38 @@ type User = {
   };
 };
 
-const getUsers = (ids: number[]): AsyncIterable<User> => {
-  return {
-    [Symbol.asyncIterator]() {
-      let i = 0;
-      return {
-        async next() {
-          console.log("getUsers next");
-          if (i === ids.length) {
-            return { done: true, value: null };
-          }
-          const data = await fetch(
-            `https://reqres.in/api/users/${ids[i++]}`
-          ).then(res => res.json());
-          return { done: false, value: data };
-        },
-      };
-    },
-  };
-};
-
-(async function () {
-  for await (const user of getUsers([1, 2, 3, 4, 5])) {
-    console.log(user);
+async function* getUsers(ids: number[]) {
+  for (const id of ids) {
+    console.log("getUsers next");
+    const data = fetch(`https://reqres.in/api/users/${id}`).then(res =>
+      res.json()
+    );
+    yield data;
   }
-})();
+}
 
-(async function () {
-  const iterator = getUsers([1, 2, 3, 4, 5])[Symbol.asyncIterator]();
-  while (true) {
-    const { done, value } = await iterator.next();
-    if (done) break;
-    console.log(value);
+// (async function () {
+//   for await (const user of getUsers([1, 2, 3, 4, 5])) {
+//     console.log(user);
+//   }
+// })();
+
+// (async function () {
+//   const iterator = getUsers([1, 2, 3, 4, 5])[Symbol.asyncIterator]();
+//   while (true) {
+//     const { done, value } = await iterator.next();
+//     if (done) break;
+//     console.log(value);
+//   }
+// })();
+
+async function* map<T, U>(
+  asyncIterable: AsyncIterable<T>,
+  callback: (value: T) => U
+) {
+  for await (const item of asyncIterable) {
+    yield callback(item);
   }
-})();
-
-function map<T, U>(iter: AsyncIterable<T>, fn: (v: T) => U): AsyncIterable<U> {
-  return {
-    [Symbol.asyncIterator]() {
-      const iterator = iter[Symbol.asyncIterator]();
-      return {
-        async next() {
-          console.log("map next");
-          const { done, value } = await iterator.next();
-          if (done) return { done, value: null };
-          return { done, value: fn(value) };
-        },
-      };
-    },
-  };
 }
 
 (async function () {
